193a153099e02ec89ac57289bb413efb19e7b621
rodrigoemanuelde <rodrigoemanuelde@hotmail.com>
2020-12-03 17:06:55 -0300

    add principal route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use("/", () => {
+  console.log("Ruta principal de la app");
+});
+
 app.listen(PORT, () => {
   console.log(`App on port ${PORT}`);
 });
